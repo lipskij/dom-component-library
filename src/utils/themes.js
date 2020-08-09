e84f74b49064eab2bffa8dd2595a18b3959a6ee1
lipskij <emil.lipskij@gmail.com>
2020-08-09 19:13:29 +0300

    unfineished library

---
+++ src/utils/themes.js
@@ -0,0 +1,12 @@
+import { purple, neutral } from './colors';
+import { primaryFont } from './typograpfy';
+
+export const defaultTheme = {
+  primaryColor: purple[300],
+  primaryColorHover: purple[200],
+  primaryColorActive: purple[100],
+  textColorOnPrimary: neutral[100],
+  textColor: neutral[600],
+  textColorInverted: neutral[100],
+  primaryFont
+}
