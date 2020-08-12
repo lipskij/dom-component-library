05e2efca0146569e630063a4e874e0545d62cbc4
lipskij <emil.lipskij@gmail.com>
2020-08-12 15:45:23 +0300

    signup modal

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,96 +1,96 @@
 import styled from 'styled-components';
-import { defaultTheme, typeScale } from '../utils';
+import { typeScale } from '../utils';
 import { applyStyleModifiers } from 'styled-components-modifiers';
 
 const BUTTON_MODIFIERS = {
-  small: () => `
+  small: ({ props }) => `
     font-size: ${typeScale.helperText};
     padding: 8px;
   `,
-  large: () => `
+  large: ({ props }) => `
     font-size: ${typeScale.header5};
     padding: 16px 24px;
   `,
-  warning: () => `
-    background-color: ${defaultTheme.status.warningColor};
-    color: ${defaultTheme.status.textColorInverted};
+  warning: ({ props }) => `
+    background-color: ${props.theme.status.warningColor};
+    color: ${props.theme.status.textColorInverted};
 
     &:hover, &:focus {
-      background-color: ${defaultTheme.status.warningColorHover};
-      outline: 3px solid ${defaultTheme.status.warningColorHover};
+      background-color: ${props.theme.status.warningColorHover};
+      outline: 3px solid ${props.theme.status.warningColorHover};
       outline-offset: 2px;
     }
 
     &:active {
-      background-color: ${defaultTheme.status.warningColorActive};
+      background-color: ${props.theme.status.warningColorActive};
     }
   `,
-  primaryButtonWarning: () => `
-    background-color: ${defaultTheme.status.warningColor};
-    color: ${defaultTheme.textColorInverted};
+  primaryButtonWarning: ({ props }) => `
+    background-color: ${props.theme.status.warningColor};
+    color: ${props.theme.textColorInverted};
   `,
-  secondaryButtonWarning: () => `
-    border: 2px solid ${defaultTheme.status.warningColor}
+  secondaryButtonWarning: ({ props }) => `
+    border: 2px solid ${props.theme.status.warningColor}
   `,
-  tertiaryButtonWarning: () => `
+  tertiaryButtonWarning: ({ props }) => `
     background: none;
-    color: ${defaultTheme.status.warningColor};
+    color: ${props.theme.status.warningColor};
   `,
 
-  error: () => `
-  background-color: ${defaultTheme.status.errorColor};
-  color: ${defaultTheme.status.textColorInverted};
+  error: ({ props }) => `
+  background-color: ${props.theme.status.errorColor};
+  color: ${props.theme.status.textColorInverted};
 
   &:hover, &:focus {
-    background-color: ${defaultTheme.status.errorColorHover};
-    outline: 3px solid ${defaultTheme.status.errorColorHover};
+    background-color: ${props.theme.status.errorColorHover};
+    outline: 3px solid ${props.theme.status.errorColorHover};
     outline-offset: 2px;
   }
 
   &:active {
-    background-color: ${defaultTheme.status.errorColorActive};
+    background-color: ${props.theme.status.errorColorActive};
   }
   `,
-  primaryErrorButton: () => `
-    background-color: ${defaultTheme.status.errorColor};
-    color: ${defaultTheme.textColorInverted};
+  primaryErrorButton: ({ props }) => `
+    background-color: ${props.theme.status.errorColor};
+    color: ${props.theme.textColorInverted};
   `,
-  secondaryErrorButton: () => `
+  secondaryErrorButton: ({ props }) => `
     background: none;
-    border: 2px solid ${defaultTheme.status.errorColor};
-    color: ${defaultTheme.status.errorColor};
+    border: 2px solid ${props.theme.status.errorColor};
+    color: ${props.theme.status.errorColor};
   `,
-  tertiaryErrorButton: () => `
+  tertiaryErrorButton: ({ props }) => `
     background: none;
-    color: ${defaultTheme.status.errorColor};
+    color: ${props.theme.status.errorColor};
   `,
 
-  success: () => `
-  background-color: ${defaultTheme.status.successColor};
-  color: ${defaultTheme.status.textColorInverted};
+  success: ({ props }) => `
+  background-color: ${props.theme.status.successColor};
+  color: ${props.theme.status.textColorInverted};
 
   &:hover, &:focus {
-    background-color: ${defaultTheme.status.successColorHover};
-    outline: 3px solid ${defaultTheme.status.successColorHover};
+    background-color: ${props.theme.status.successColorHover};
+    outline: 3px solid ${props.theme.status.successColorHover};
     outline-offset: 2px;
   }
 
   &:active {
-    background-color: ${defaultTheme.status.successColorActive};
+    background-color: ${props.theme.status.successColorActive};
   }
   `,
-  primarySuccessButton: () => `
-    background-color: ${defaultTheme.status.successColor};
-    color: ${defaultTheme.textColorInverted};
+  primarySuccessButton: ({ props }) => `
+    background-color: ${props.theme.status.successColor};
+    color: ${props.theme.textColorInverted};
   `,
-  secondarySuccessButton: () => `
+  secondarySuccessButton: ({ props }) => `
     background: none;
-    border: 2px solid ${defaultTheme.status.successColor};
-    color: ${defaultTheme.status.successColor};
+    border: 2px solid ${props.theme.status.successColor};
+    color: ${props.theme.status.successColor};
   `,
-  tertiarySuccessButton: () => `
+  tertiarySuccessButton: ({ props }) => `
     background: none;
-    color: ${defaultTheme.status.successColor};
+    color: ${props.theme.status.successColor};
   `,
 };
 
@@ -104,30 +104,30 @@ const Button = styled.button`
   transition: background-color 0.2s linear, color 0.2s linear;
 
   &:hover {
-    background-color: ${defaultTheme.primaryColorHover};
-    color: ${defaultTheme.textColorOnPrimary};
+    background-color: ${props => props.theme.primaryColorHover};
+    color: ${props => props.theme.textColorOnPrimary};
   }
 
   &:focus {
-    outline: 2px solid ${defaultTheme.primaryColorHover};
+    outline: 2px solid ${props => props.theme.primaryColorHover};
     outine-offset: 2px;
   }
 
   &:active {
-    background-color: ${defaultTheme.primaryColorActive};
-    border-color: ${defaultTheme.primaryColorActive};
-    color: ${defaultTheme.textColorOnPrimary};
+    background-color: ${props => props.theme.primaryColorActive};
+    border-color: ${props => props.theme.primaryColorActive};
+    color: ${props => props.theme.textColorOnPrimary};
   }
 `;
 
-const PrimaryButton = styled(Button)`
-  background-color: ${defaultTheme.primaryColor};
+export const PrimaryButton = styled(Button)`
+  background-color: ${props => props.theme.primaryColor};
   border: none;
-  color: white;
+  color: ${props => props.theme.textColorOnPrimary};
 
   &:disabled {
-    background-color: ${defaultTheme.disabled};
-    color: ${defaultTheme.textOnDisabled};
+    background-color: ${props => props.theme.disabled};
+    color: ${props => props.theme.textOnDisabled};
     cursor: not-allowed;
   }
 
@@ -136,13 +136,13 @@ const PrimaryButton = styled(Button)`
 
 export const SecondaryButton = styled(Button)`
 background: none;
-border: 2px solid ${defaultTheme.primaryColor};
-border-color: ${defaultTheme.disabled};
-color: ${defaultTheme.primaryColor};
+border: 2px solid ${props => props.theme.primaryColor};
+color: ${props => props.theme.primaryColor};
 
 &:disabled {
   background: none;
-  color: ${defaultTheme.disabled};
+  color: ${props => props.theme.disabled};
+  border-color: ${props => props.theme.disabled};
   cursor: not-allowed;
 }
 
@@ -152,15 +152,13 @@ ${applyStyleModifiers(BUTTON_MODIFIERS)}
 export const TertiaryButton = styled(Button)`
 background: none;
 border: none;
-color: ${defaultTheme.primaryColor};
+color: ${props => props.theme.primaryColor};
 
 &:disabled {
   background: none;
-  color: ${defaultTheme.disabled};
+  color: ${props => props.theme.disabled};
   cursor: not-allowed;
 }
 
 ${applyStyleModifiers(BUTTON_MODIFIERS)}
 `;
-
-export default PrimaryButton;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import PrimaryButton, { SecondaryButton, TertiaryButton } from './components/Buttons';
-import { GlobalStyle } from './utils';
+import { ThemeProvider } from 'styled-components';
+import { SignUpModal } from './components';
+import { GlobalStyle, darkTheme, defaultTheme } from './utils';
 
-const App = () => (
-  <div>
-    <PrimaryButton modifiers={["small", "success", "primarySuccessButton"]}>Hello</PrimaryButton>
-    <SecondaryButton modifiers={["large", "warning", "secondaryButtonWarning"]}>Hello You</SecondaryButton>
-    <TertiaryButton modifiers={["error", "tertiaryErrorButton"]}>Hello Boi</TertiaryButton>
-    <GlobalStyle />
-  </div>
-);
+const App = () => {
+  const [useDarkTheme, setUseDarkTheme] = useState(false);
+  return (
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      <button style={{ margin: "0 16px 24px", padding: "8px", background: "none" }} onClick={() => setUseDarkTheme(true)}>
+        Dark Theme
+      </button>
+      <button style={{ margin: "0 16px 24px", padding: "8px", background: "none" }} onClick={() => setUseDarkTheme(false)}>
+        Default Theme
+      </button>
+      <div style={{
+        background: useDarkTheme ? defaultTheme.primaryColor : darkTheme.primaryColor, 
+        width: "100vw",
+        height: "90vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-around"
+      }}>
+        <SignUpModal />
+      <GlobalStyle />
+      </div>
+    </ThemeProvider>
+  );
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
