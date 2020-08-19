aaae4edfbbada43e6423fd6472e9130ae5d503ef
lipskij <emil.lipskij@gmail.com>
2020-08-19 13:23:03 +0300

    Adding story

---
+++ storybook-static/precache-manifest.9b725e5e5ffa054d7b94d760742d31a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa37bb5a41a21271b527cd100461baa1",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.bfa93d87958604eb2e37.bundle.js"
+  },
+  {
+    "url": "runtime~main.bfa93d87958604eb2e37.bundle.js"
+  },
+  {
+    "revision": "ec6d4d6ea17b9039d99f73e94f511041",
+    "url": "static/media/sign-up.ec6d4d6e.svg"
+  },
+  {
+    "url": "vendors~main.bfa93d87958604eb2e37.bundle.js"
+  },
+  {
+    "url": "vendors~main.bfa93d87958604eb2e37.bundle.js.LICENSE.txt"
+  }
+]);
